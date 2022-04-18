8b2c36307545b3d14474931261ba19f8ad314396
Laura <lauramjohnson@me.com>
2022-04-18 17:52:24 -0500

    Initial Commit

---
+++ Cool Cats/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cool Cats
+//
+//  Created by Laura Johnson on 4/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Cool Cats/Cool_CatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cool_CatsApp.swift
+//  Cool Cats
+//
+//  Created by Laura Johnson on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct Cool_CatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
